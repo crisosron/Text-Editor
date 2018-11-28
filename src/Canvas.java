5baf7bdcd4fd3b0551b3ba515f4157b7d79d383b
Ron Crisostomo <rcrisostomo97@gmail.com>
2018-11-28 19:48:02 +1300

    Began open and save development on PaintWindow

--- src/Canvas.java
+++ src/Canvas.java
@@ -6,13 +6,15 @@ import java.awt.event.MouseMotionListener;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
+import java.io.File;
+import java.io.PrintStream;
 import java.util.*;
 import java.util.List;
 
 public class Canvas extends JPanel implements MouseListener, MouseMotionListener {
     private int x, y, width, height;
     float eraserWidth, eraserHeight;
-    private List<ShapeItem> shapeItems, whiteEraserCircleShapeItems;
+    private List<ShapeItem> shapeItems, whiteEraserCircleShapeItems, lineShapes; //shapeItems is the master list, lineShapes is used as a checker if a shape is a line
     private Point shapeStart, shapeEnd;
 
     public Canvas(int x, int y, int width, int height){
@@ -22,6 +24,7 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
         this.height = height;
         shapeItems = new ArrayList<>();
         whiteEraserCircleShapeItems = new ArrayList<>();
+        lineShapes = new ArrayList<>();
         eraserWidth = 50;
         eraserHeight = 50;
         setupCanvas();
@@ -48,7 +51,7 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
         /*Drawing all the shapes*/
         for(ShapeItem shapeItem : shapeItems){
             graphics2D.setColor(shapeItem.getShapeColor());
-            if(!shapeItem.isFilled()) {
+            if(!shapeItem.isFilled() || lineShapes.contains(shapeItem)) {
                 graphics2D.draw(shapeItem.getShape());
             }else{
                 graphics2D.fill(shapeItem.getShape());
@@ -85,41 +88,49 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
     /**
      * Creates a rectangle shape and adds to list
      */
-    public void createRect(int startX, int startY, int endX, int endY){
+    public void createRect(int startX, int startY, int endX, int endY, boolean hasFill, Color color){
         int x = Math.min(startX, endX);
         int y = Math.min(startY, endY);
         int width = Math.abs(endX - startX);
         int height = Math.abs(endY - startY);
         Rectangle2D.Float newRect = new Rectangle2D.Float(x, y, width, height);
-        ShapeItem shapeItem = new ShapeItem(newRect, TextEditor.paintWindow.selectedColor, TextEditor.paintWindow.filling);
+        Point firstPoint = new Point(startX, startY);
+        Point secondPoint = new Point(endX, endY);
+        ShapeItem shapeItem = new ShapeItem(newRect, TextEditor.paintWindow.selectedColor, hasFill, firstPoint, secondPoint, "Rectangle");
         shapeItems.add(shapeItem);
     }
 
     /**
      * Creates a line shape and adds to list
      */
-    public void createLine(int startX, int startY, int endX, int endY){
+    public void createLine(int startX, int startY, int endX, int endY, Color color){
         Line2D.Float newLine = new Line2D.Float(startX, startY, endX, endY);
-        ShapeItem shapeItem = new ShapeItem(newLine, TextEditor.paintWindow.selectedColor, TextEditor.paintWindow.filling);
+        Point firstPoint = new Point(startX, startY);
+        Point secondPoint = new Point(endX, endY);
+        ShapeItem shapeItem = new ShapeItem(newLine, TextEditor.paintWindow.selectedColor, false, firstPoint, secondPoint, "Line"); //Line has no fill by default
         shapeItems.add(shapeItem);
+        lineShapes.add(shapeItem);
      }
 
     /**
      * Creates an oval shape and adds to list
      */
-    public void createOval(int startX, int startY, int endX, int endY){
+    public void createOval(int startX, int startY, int endX, int endY, boolean hasFill, Color color){
         int x = Math.min(startX, endX);
         int y = Math.min(startY, endY);
         int width = Math.abs(endX - startX);
         int height = Math.abs(endY - startY);
         Ellipse2D.Float newOval = new Ellipse2D.Float(x, y, width, height);
-        ShapeItem shapeItem = new ShapeItem(newOval, TextEditor.paintWindow.selectedColor, TextEditor.paintWindow.filling);
+        Point firstPoint = new Point(startX, startY);
+        Point secondPoint = new Point(endX, endY);
+        ShapeItem shapeItem = new ShapeItem(newOval, TextEditor.paintWindow.selectedColor, hasFill, firstPoint, secondPoint, "Oval");
         shapeItems.add(shapeItem);
     }
 
     /**
      * Creates a white circle to emulate erasing
      */
+    /*
     public void createEraseCircle(){
         float x = shapeStart.x - eraserWidth/2;
         float y = shapeStart.y - eraserHeight/2;
@@ -128,6 +139,7 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
         shapeItems.add(shapeItem);
         whiteEraserCircleShapeItems.add(shapeItem);
     }
+    */
 
     public void mouseClicked(MouseEvent mouseEvent){}
 
@@ -144,9 +156,9 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
      * When the mouse is released, that is when the shape is added to the shapes list for drawing
      */
     public void mouseReleased(MouseEvent mouseEvent){
-        if(TextEditor.paintWindow.selectedTool.equals("line")) createLine(shapeStart.x, shapeStart.y, shapeEnd.x, shapeEnd.y);
-        else if(TextEditor.paintWindow.selectedTool.equals("rectangle")) createRect(shapeStart.x, shapeStart.y, shapeEnd.x, shapeEnd.y);
-        else if(TextEditor.paintWindow.selectedTool.equals("ellipse")) createOval(shapeStart.x, shapeStart.y, shapeEnd.x, shapeEnd.y);
+        if(TextEditor.paintWindow.selectedTool.equals("line")) createLine(shapeStart.x, shapeStart.y, shapeEnd.x, shapeEnd.y, TextEditor.paintWindow.selectedColor);
+        else if(TextEditor.paintWindow.selectedTool.equals("rectangle")) createRect(shapeStart.x, shapeStart.y, shapeEnd.x, shapeEnd.y, TextEditor.paintWindow.filling, TextEditor.paintWindow.selectedColor);
+        else if(TextEditor.paintWindow.selectedTool.equals("ellipse")) createOval(shapeStart.x, shapeStart.y, shapeEnd.x, shapeEnd.y, TextEditor.paintWindow.filling, TextEditor.paintWindow.selectedColor);
         repaint();
     }
 
@@ -163,10 +175,92 @@ public class Canvas extends JPanel implements MouseListener, MouseMotionListener
     }
 
     public void createNew(){}
-    public void save(){}
+
+    /**
+     * Saves all shapes to a file
+     */
+    public void save(){
+       try{
+
+           /*File object to write to and PrintStream object to write to the file*/
+           File saveToFile = new File("paint.txt");
+           PrintStream printStreamWriter = new PrintStream(saveToFile);
+           //TODO: Make it so that the save as dialog appears and allows the user to name their file
+
+           /*Looping through shapeItems to write each shape's info to the file*/
+           for(ShapeItem shapeItem : shapeItems){
+               String shapeType = shapeItem.getShapeType();
+               double x1 = shapeItem.getPointOne().getX();
+               double y1 = shapeItem.getPointOne().getY();
+               double x2 = shapeItem.getPointTwo().getX();
+               double y2 = shapeItem.getPointTwo().getY();
+               Color shapeColor = shapeItem.getShapeColor();
+               int red = shapeColor.getRed();
+               int blue = shapeColor.getBlue();
+               int green = shapeColor.getGreen();
+               boolean hasFill = shapeItem.isFilled();
+               printStreamWriter.println(shapeType + " " + red + " " + green + " " + blue + " " + hasFill + " " + x1 + " " + y1 + " " + x2 + " " + y2);
+           }
+       } catch(Exception e){e.printStackTrace();}
+    }
+
     public void clear(){
         shapeItems.clear();
-        repaint();
+        Graphics g = getGraphics();
+        super.paintComponent(g); //Clears the JPanel
     }
-    public void open(){}
+
+    /**
+     * Open's a file with shape info
+     */
+    //TODO: Make it check if the file is a valid shapes file or not (maybe write to the file that the file is indeed a shapes file and check if the apppears here?)
+    public void open(){
+        clear();
+        try{
+
+            /*Prompting the user to open a file using a JFileChooser object*/
+            JFileChooser openFileChooser = new JFileChooser();
+            int status = openFileChooser.showOpenDialog(null);
+            if(status != JFileChooser.APPROVE_OPTION){
+                JOptionPane.showMessageDialog(null, "No File Selected!");
+                return;
+            }
+            File selectedFile = openFileChooser.getSelectedFile();
+            Scanner scanFile = new Scanner(selectedFile);
+
+            /*Scanning the file to get the shapes to display*/
+            while(scanFile.hasNext()){
+
+                /*Type of shape*/
+                String shapeType = scanFile.next();
+
+                /*RGB values of the shape color*/
+                int red = scanFile.nextInt();
+                int green = scanFile.nextInt();
+                int blue = scanFile.nextInt();
+
+                /*Boolean that dictates if the shape is filled or not*/
+                boolean shapeIsFilled = scanFile.nextBoolean();
+
+                /*X and Y values of the shape*/
+                int x1 = (int)scanFile.nextDouble();
+                int y1 = (int)scanFile.nextDouble();
+                int x2 = (int)scanFile.nextDouble();
+                int y2 = (int)scanFile.nextDouble();
+
+                /*Creating the color of the shape*/
+                Color shapeColor = new Color(red, green, blue);
+
+                /*Creating the shape based on the type of shape*/
+                if(shapeType.equals("Line")) createLine(x1, y1, x2, y2, shapeColor);
+                else if(shapeType.equals("Oval")) createOval(x1, y1, x2, y2, shapeIsFilled, shapeColor);
+                else if(shapeType.equals("Rectangle")) createRect(x1, y1, x2, y2, shapeIsFilled, shapeColor);
+                repaint();
+
+                //TODO: Fix so that each shape uses its own individual color
+            }
+
+        }catch(Exception e){e.printStackTrace();}
+
+    }
 }
--- src/ShapeItem.java
+++ src/ShapeItem.java
@@ -7,12 +7,21 @@ public class ShapeItem {
     private Shape shape;
     private Color color;
     private boolean hasFill;
-    public ShapeItem(Shape shape, Color color, boolean hasFill){
+    private Point pointOne, pointTwo; //Point objects are used to make it easier to manage x and y values when loading shapes into window from a file, and saving shapes to a file
+    private String shapeType;
+    public ShapeItem(Shape shape, Color color, boolean hasFill, Point pointOne, Point pointTwo, String shapeType){
         this.shape = shape;
         this.color = color;
         this.hasFill = hasFill;
+        this.pointOne = pointOne;
+        this.pointTwo = pointTwo;
+        this.shapeType = shapeType;
     }
     public Shape getShape(){ return shape;}
     public Color getShapeColor(){return color;}
     public boolean isFilled(){return hasFill;}
+    public Point getPointOne(){return pointOne;}
+    public Point getPointTwo(){return pointTwo;}
+    public String getShapeType(){return shapeType;}
+
 }
